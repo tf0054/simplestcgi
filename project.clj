21fe9e65478161b6f20c8a44aee0a3039f1867fb
Takeshi Nakano <tf0054@gmail.com>
2014-12-24 15:21:30 +0100

    Replaced ring to smaller ring-core.

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [
                  [org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
-                 [ring "1.3.2"]
+                 [ring/ring-core "1.3.2"]
                  [http-kit "2.1.16"]
   ]
   :main simplestcgi.core
